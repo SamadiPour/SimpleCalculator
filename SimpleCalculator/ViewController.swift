099f5ae68fa0cfd8e7399ce7b840598c98e46c3a
Amir Hossein <samadipour@Amirs-Mac.local>
2019-02-10 08:14:12 +0330

    Now it can add text

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -10,11 +10,128 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*----------------------Labels----------------------*/
+    @IBOutlet weak var inputLable: UILabel!
+    @IBOutlet weak var resultLable: UILabel!
+    
+    
+    
+    /*----------------------operator enum----------------------*/
+    enum Operators : String {
+        case plus = "+"
+        case minus = "-"
+        case divide = "/"
+        case multiply = "*"
+    }
+    
+    
+    
+    /*----------------------Variables----------------------*/
+    var numbersArray : [Double] = [0]
+    var numbersStringArray : [String] = ["0"]
+    var operatorsArray : [Operators] = [Operators.plus]
+    var inputText : String = "0"
+    var numberExist : Bool = false
+    var pointExist : Bool = false
+    
+    
+    
+    /*----------------------Main Function----------------------*/
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
+    /*----------------------Main Operations!----------------------*/
+    @IBAction func clearAll(_ sender: UIButton) {
+    }
+    
+    @IBAction func calculateBtn(_ sender: UIButton) {
+    }
+    
+    
+    
+    /*----------------------Operators----------------------*/
+    @IBAction func mainMathOperators(_ sender: UIButton) {
+        //reset variables
+        numberExist = false
+        pointExist = false
+        numbersStringArray.append("")
+        
+        //get number that clicked
+        guard let inputString : String = sender.titleLabel?.text else {return}
+        
+        //modify input
+        inputText += " " + inputString + " "
+        
+        //set lable with new input
+        inputLable.text = inputText
+        
+        //find operator and add it to array
+        guard let ops : Operators = Operators.init(rawValue: inputString) else {return}
+        operatorsArray.append(ops)
+    }
+    
+    @IBAction func signBtn(_ sender: Any) {
+    }
+    
+    @IBAction func percentBtn(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    /*----------------------Numbers And Point----------------------*/
+    @IBAction func digitButtons(_ sender: UIButton) {
+        //get number that clicked
+        guard let inputString : String = sender.titleLabel?.text else {return}
+        
+        //check if it is a point
+        if inputString == "." {
+            if pointExist {
+                return
+            } else {
+                pointExist = true
+            }
+        }
+        
+        //pop last number to modify it
+        guard let numberString : String = numbersStringArray.removeLast() else {return}
+        if numberExist {
+            numbersArray.removeLast()
+        }
+        
+        //modify number based on input
+        let newNumberString : String
+        if inputText == "0" && inputString != "." {
+            newNumberString = inputString
+        } else {
+            newNumberString = numberString + inputString
+        }
+        
+        //push back new value
+        numbersStringArray.append(newNumberString)
+        numbersArray.append(Double(newNumberString)!)
+        
+        //change input based on new number
+        inputText = inputText.prefix(inputText.count - numberString.count) + newNumberString
+        print(numberString, ":", inputText)
+        
+        //re calculate whole input
+        numberExist = true
+        reCalculate()
+    }
+    
+    func reCalculate(){
+        //set lable with new input
+        inputLable.text = inputText
+        
+        //if we have 2 number in our list and we are sure that next number is entered then calculate result
+        if (numbersArray.count) > 1 && numberExist {
+            
+        }
+        
+    }
+    
 }
-
